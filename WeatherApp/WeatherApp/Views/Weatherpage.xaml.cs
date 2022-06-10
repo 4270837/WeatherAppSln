0f890b2487d7820c42ce40d2ca6fce349b1667d9
Ashley <4270837@myuwc.ac.za>
2022-06-10 12:42:20 +0200

    cs on weatherpage

---
+++ WeatherApp/WeatherApp/Views/Weatherpage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Weatherpage : ContentPage
+    {
+        public Weatherpage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await GetWeathersData();
+        }
+        private async Task GetWeathersData()
+        {
+            var data = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (data != PermissionStatus.Granted)
+            {
+                var newdata = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+            var location = await Geolocation.GetLocationAsync();
+            var latitude = location.Latitude; 
+            var longitude = location.Longitude;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid=55d3380a729f5854eb8d5adc906d1122";
+            var response = await client.GetStringAsync(url);
+            var weathersData = JsonConvert.DeserializeObject<WeatherData>(response);
+            BindingContext = weathersData;
+        }
+    }
+}
